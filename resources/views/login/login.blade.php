02d49def5b0b7259b86a0889c3979e31e6c096c9
luckly123456 <1607384710@qq.com>
2019-02-24 08:51:00 +0800

    提交

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -13,7 +13,11 @@
             <label for="password">密码：</label>
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
         </div>
-
+        <div class="form-group">
+            <label>验证码</label>
+            <input type="text" name="captcha" class="form-control" style="width: 200px">
+            <img class="thumbnail captcha" src="{{ captcha_src('default') }}" onclick="this.src='/captcha/default?'+Math.random()" title="点击图片重新获取验证码">
+        </div>
         <div class="checkbox">
             <label>
                 <input type="checkbox" name="rememberMe" value="1">记住我哦
--- resources/views/login/sign.blade.php
+++ resources/views/login/sign.blade.php
@@ -1,9 +1,9 @@
 @extends('layout.app')
 
 @section('contents')
+    <h1>商家注册</h1>
     <form method="post" action="">
         {{ csrf_field() }}
-
         <div class="form-group">
             <label for="email">请输入用户名：</label>
             <input type="text" name="name" class="form-control" value="{{ old('name') }}">
@@ -15,11 +15,26 @@
         </div>
         <div class="form-group">
             <label for="password">请确认密码：</label>
-            <input type="password" name="password_confirmation " class="form-control" value="{{ old('password2') }}">
+            <input type="password" name="password2" class="form-control" value="{{ old('password2') }}">
         </div>
         <div class="form-group">
             <label for="password">请输入邮箱：</label>
             <input type="email" name="email" class="form-control" value="{{ old('email') }}">
+        </div>
+        <div class="form-group">
+            <label>所属商家</label>
+            <select name="shop_id" class="form-control" style="width: 200px">
+                @foreach($shops as $shop)
+                    <option value="{{ $shop->id }}"
+                            @if(old('$shop_id')==$shop->id) selected @endif
+                    >{{ $shop->shop_name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="form-group">
+            <label>验证码</label>
+            <input type="text" name="captcha" class="form-control" style="width: 200px">
+            <img class="thumbnail captcha" src="{{ captcha_src('default') }}" onclick="this.src='/captcha/default?'+Math.random()" title="点击图片重新获取验证码">
         </div>
         <button type="submit" class="btn btn-primary">登录</button>
     </form>
